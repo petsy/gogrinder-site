6181d8af7ea9a866b0a272bb26efda77d526f37a
mark <mark@mark-fink.de>
2016-01-02 14:54:58 +0100

    adjusted static paths

--- themes/gogrinder/static/js/lightbox.js
+++ themes/gogrinder/static/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = 'theme/img/loading.gif';
+      this.fileCloseImage = 'theme/img/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
